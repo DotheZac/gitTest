8fa0e2b5cda47be5de0b0648e4578aaddd3336ae
DotheZac <dothezac21@gmail.com>
2023-05-23 21:36:05 +0900

    djvue test

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="recipe in recipes" :key="recipe.id">
+        {{ recipe.id }} - {{ recipe.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      recipes: [],
+    };
+  },
+  mounted() {
+    this.fetchRecipes();
+  },
+  methods: {
+    fetchRecipes() {
+      const apiUrl = '/modu-calorie-backend-main/api/recipes_file';
+
+      axios.get(apiUrl)
+        .then(response => {
+          this.recipes = response.data;
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    },
+  },
+};
+</script>
+
+
+<style>
+
+</style>
